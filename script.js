e510805fbef941d954278f1c8790f3a192bd11b3
KovasMcCann <KovasMcCann@outlook.com>
2024-08-09 14:32:48 -0500

    clean up

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 // Based off of https://github.com/martinshkreli/stocks
-
-/* const tickers = [
-    'AAPL', 'ABBV', 'AMD', 'BA', 'BABA', 'BAC', 'BAX', 'BBY', 'BDX', 'BIDU', 'BIIB',
-    'BMY', 'BP', 'BRK.A', 'BRK.B', 'BSX', 'C', 'CL', 'CLFD', 'DIS', 'EA', 'F', 'GM',
-    'GS', 'HD', 'IBM', 'JNJ', 'KO', 'LMT', 'LOW', 'META', 'MSFT', 'NFLX', 'NKE',
-    'NOK', 'NVDA', 'ORCL', 'OXY', 'PFE', 'PG', 'PYPL', 'QCOM', 'RBLX', 'REGN',
-    'SBUX', 'SHOP', 'SQ', 'T', 'TSLA', 'U', 'UNH', 'V', 'VZ', 'WMT', 'XOM', 'ZM'
-]; */
-
 const tickers = [ 'AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'FB', 'NVDA', 'PYPL', 'NFLX', 'INTC', 'CSCO', 'CMCSA', 'PEP', 'ADBE', 'AVGO', 'TMUS', 'COST', 'QCOM', 'AMGN', 'TXN', 'SBUX', 'INTU', 'AMD', 'ISRG', 'GILD', 'MU', 'ADP', 'BKNG' ];
 
 let count = 0;
@@ -54,29 +45,6 @@ async function grab() {
             }
 
             count++;
-            /*
-            if (count % 250 === 0 && count > 1) {
-                const endTime = Date.now();
-                const elapsedTime = (endTime - startTime) / 1000;
-                const seconds = Math.floor(elapsedTime % 60);
-                const minutes = Math.floor(elapsedTime / 60);
-                const rate = Math.floor(count / elapsedTime);
-
-                const status = document.getElementById('status');
-                status.style.position = 'fixed';
-                status.style.bottom = '10px';
-                status.style.left = '10px';
-                status.style.color = '#FF00FF'; // Neon pink
-                status.style.backgroundColor = '#000000'; // Black background
-                status.style.padding = '10px';
-                status.style.borderRadius = '5px';
-                status.innerHTML = `
-                    Data Received: ${count} <br>
-                    Time Elapsed: ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} <br>
-                    Rate: ${rate}x
-                `;
-            }
-            */
         } catch (error) {
             console.error('Error fetching data:', error);
         }
